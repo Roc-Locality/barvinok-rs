f659ae5284faade9731ba6a94e24110eb35e0359
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-04-23 22:24:27 -0400

    feat(set): basic aset and set definitions

--- barvinok-sys/build.rs
+++ barvinok-sys/build.rs
@@ -19,6 +19,7 @@ fn main() {
         .header(format!("{}/include/isl/val.h", dst.display()))
         .header(format!("{}/include/isl/space.h", dst.display()))
         .header(format!("{}/include/isl/id.h", dst.display()))
+        .header(format!("{}/include/isl/set.h", dst.display()))
         .clang_arg(format!("-I{}", include_dir.display()))
         // allow only those functions starts with barvinok and isl and recursively
         .allowlist_function("isl.*")
--- barvinok/src/set.rs
+++ barvinok/src/set.rs
@@ -1,1 +1,62 @@
+use std::ptr::NonNull;
 
+#[repr(transparent)]
+pub struct BasicSet<'a> {
+    pub(crate) handle: NonNull<barvinok_sys::isl_basic_set>,
+    pub(crate) marker: std::marker::PhantomData<*mut &'a ()>,
+}
+
+#[repr(transparent)]
+pub struct Set<'a> {
+    pub(crate) handle: NonNull<barvinok_sys::isl_set>,
+    pub(crate) marker: std::marker::PhantomData<*mut &'a ()>,
+}
+
+impl<'a> Set<'a> {
+    pub fn context_ref(&self) -> crate::ContextRef<'a> {
+        let handle = unsafe { barvinok_sys::isl_set_get_ctx(self.handle.as_ptr()) };
+        let handle = unsafe { NonNull::new_unchecked(handle) };
+        crate::ContextRef(handle, std::marker::PhantomData)
+    }
+}
+
+impl Drop for Set<'_> {
+    fn drop(&mut self) {
+        unsafe { barvinok_sys::isl_set_free(self.handle.as_ptr()) };
+    }
+}
+impl Clone for Set<'_> {
+    fn clone(&self) -> Self {
+        let handle = unsafe { barvinok_sys::isl_set_copy(self.handle.as_ptr()) };
+        let handle = unsafe { NonNull::new_unchecked(handle) };
+        Self {
+            handle,
+            marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> BasicSet<'a> {
+    pub fn context_ref(&self) -> crate::ContextRef<'a> {
+        let handle = unsafe { barvinok_sys::isl_basic_set_get_ctx(self.handle.as_ptr()) };
+        let handle = unsafe { NonNull::new_unchecked(handle) };
+        crate::ContextRef(handle, std::marker::PhantomData)
+    }
+}
+
+impl Drop for BasicSet<'_> {
+    fn drop(&mut self) {
+        unsafe { barvinok_sys::isl_basic_set_free(self.handle.as_ptr()) };
+    }
+}
+
+impl Clone for BasicSet<'_> {
+    fn clone(&self) -> Self {
+        let handle = unsafe { barvinok_sys::isl_basic_set_copy(self.handle.as_ptr()) };
+        let handle = unsafe { NonNull::new_unchecked(handle) };
+        Self {
+            handle,
+            marker: std::marker::PhantomData,
+        }
+    }
+}
